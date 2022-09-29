ffe08bdb6d08340db1d972252d03e8caefe12169
ponka578 <ponka578@su25-302.ad.liu.se>
2022-09-29 13:38:05 +0200

    more merge shit

--- train_dmi/button_handler.cpp
+++ train_dmi/button_handler.cpp
@@ -8,18 +8,5 @@ void Button_Handler::pontUpClicked()
     qDebug() << "Pontograph Up!";
     QJsonObject json{};
     json.insert(VTI_DMI::PONTOGRAPH_UP, true);
-<<<<<<< HEAD
-    QObject *obj = rootObject->findChild<QObject*>(VTI_DMI::PONTOGRAPH_UP);
-    if(obj->property("state")=="on")
-    {
-        obj->setProperty("state","off");
-    }
-    else
-    {
-        obj->setProperty("state","on");
-    }
-
-=======
->>>>>>> 5fdf118bb576aa33b91dbd3db41ae2799dd131d2
     emit sendUpdate(json);
 }
